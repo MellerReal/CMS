3712b7379a527e1ea2a5d77f9d870381f38ab8bb
MellerReal <LoyCityTeknik@gmail.com>
2015-11-18 19:54:28 +0100

    Update _pdo.php

--- _class/_pdo.php
+++ _class/_pdo.php
@@ -11,13 +11,26 @@ else
     return true;
 }
 
-if ($m_d = new PDO('mysql:host=' . Config::read('pdo_Hostname') . ';dbname=' . Config::read('pdo_Database'), Config::read('pdo_Username'), Config::read('pdo_Password')))
+class Core
 {
-    return true;
-}
-else
-{
-    Err('_class/_pdo.php', '14');
-    
+    public $core; // handle of the db connexion
+    private static $instance;
+
+    private function __construct()
+    {
+        $dsn = 'pgsql:host=' . Config::read('pdo_Hostname') . ';dbname=' . Config::read('pdo_Database') . ';port=' . Config::read('pdo_Port') . ';connect_timeout=15';
+        $this->core = new PDO($dsn, Config::read('pdo_Username'), Config::read('pdo_Password'));
+    }
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance))
+        {
+            $object = __CLASS__;
+            self::$instance = new $object;
+        }
+        return self::$instance;
+    }
+
 }
 ?>
