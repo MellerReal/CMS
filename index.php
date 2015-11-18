79358471b77415ad390ad1a496be4f324337d1b7
MellerReal <LoyCityTeknik@gmail.com>
2015-11-18 19:44:43 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//Don't change this
+$Err = false;
+
+//Don't change this to "i"
+include("_functions/_i.php");
+
+//Includes using the i.php function
+i("_config/_config.php");
+i("_functions/_error.php");
+i("_functions/_login.php");
+i("_class/_pdo.php");
+?>
